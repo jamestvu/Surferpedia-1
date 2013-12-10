7214eaad34a64850d0d3a11383519a783a945825
jamestvu <jamesvu@hawaii.edu>
2013-12-09 23:28:15 -1000

    close #19

--- app/views/formdata/Countries.java
+++ app/views/formdata/Countries.java
@@ -1,23 +1,39 @@
 package views.formdata;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import models.Surfer;
+import models.SurferDB;
 
 /**
  * Represents surfer types.
  */
 
 public class Countries {
-    
-  private static String[] types = {"Male", "Female", "Grom"};
-  
+
+  /**
+   * Method that retrieves the list of all countries used in the Database.
+   * @return list of countries.
+   */
+  public static List<String> getTypesFromDB() {
+    ArrayList<String> types = new ArrayList<String>();
+    List<Surfer> surfers = SurferDB.getSurfers();
+    for(Surfer surfer : surfers) {
+      if (!types.contains(surfer.getCountry())) {
+        types.add(surfer.getCountry());
+      }
+    }
+    return types;
+  }
   /**
    * Returns an initialized Map of surfer types (when no type is selected).
    * @return The surfer type map.
    */
   public static Map<String, Boolean> getTypes() {
     Map<String, Boolean> typeMap = new HashMap<>();
-    for (String type : types) {
+    for (String type : getTypesFromDB()) {
       typeMap.put(type, false);
     }
     return typeMap;
@@ -29,7 +45,7 @@ public class Countries {
    * @return The surfer type map.
    */
   public static Map<String, Boolean> getTypes(String surfType) {
-    Map<String, Boolean> typeMap = SurferTypes.getTypes();
+    Map<String, Boolean> typeMap = Countries.getTypes();
     if (isType(surfType)) {
       typeMap.put(surfType,  true);
     }
@@ -42,6 +58,6 @@ public class Countries {
    * @return True if valid, else false.
    */
   public static boolean isType(String surfType) {
-    return SurferTypes.getTypes().keySet().contains(surfType);
+    return Countries.getTypes().keySet().contains(surfType);
   }
 }
